7fb73fa3ff5ea1f457dfc4f9ad2bb8c1ef13104f
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-11-29 17:30:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2199,7 +2199,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
   fs.writeFile("./json/awsmm.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   })
-});*/
+});
 
 /*nickname*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
